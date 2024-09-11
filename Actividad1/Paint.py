71194797fef4c10441f5bfe43f689e157cd8ab3e
Bruno Zamora <a01798275@tec.mx>
2024-09-10 19:32:41 -0600

    Circulo y nuevo color

--- Actividad1/Paint.py
+++ Actividad1/Paint.py
@@ -10,12 +10,11 @@ Exercises
 """
 
 from turtle import *
-
 from freegames import vector
 
 
 def line(start, end):
-    """Draw line from start to end."""
+    """Draw line from start to end with a random color from a predefined list."""
     up()
     goto(start.x, start.y)
     down()
@@ -38,7 +37,15 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    
+    for count in range(360):
+        forward(end.x/15 - start.x/15)
+        left(1)
+    end_fill()
 
 
 def rectangle(start, end):
@@ -116,6 +123,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('magenta'), 'M')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
